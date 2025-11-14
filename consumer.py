45e11616bc335422155e6e3bc7dab0bfc0967f50
ishworii <ishworkhanal21@gmail.com>
2025-11-14 01:36:30 -0500

    added keys for producer

--- consumer.py
+++ consumer.py
@@ -4,6 +4,9 @@ import json
 def json_deserializer(data):
     return json.loads(data.decode("utf-8"))
 
+def key_deserializer(data):
+    return data.decode("utf-8")
+
 
 consumer = KafkaConsumer(
     "events",
@@ -11,14 +14,15 @@ consumer = KafkaConsumer(
     auto_offset_reset="earliest",
     group_id="event-watchers",
     value_deserializer=json_deserializer,
+    key_deserializer = key_deserializer,
 )
 print("Starting consumer ...Waiting for messages..")
 print("-" * 20)
 try:
     for message in consumer:
         event_data = message.value
-        print(f"RECV(Partition {message.partition}):{event_data}")
-        print(f"->User {event_data['user_id']} performed action:{event_data['action']}")
+        print(f"RECV(Partition {message.partition},Key {message.key}):{event_data}")
+        # print(f"->User {event_data['user_id']} performed action:{event_data['action']}")
 except KeyboardInterrupt:
     print("\nConsumer stopped..")
 finally:
--- producer.py
+++ producer.py
@@ -6,28 +6,32 @@ import random
 def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
-producer = KafkaProducer(bootstrap_servers="localhost:9092",value_serializer=json_serializer)
+def key_serializer(data):
+    return str(data).encode("utf-8")
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092",value_serializer=json_serializer,key_serializer=key_serializer)
 
 
 print("Starting producer..")
 event_id = 1
 # actions example
 actions = ["click","view","add_to_cart","purchase"]
-
+# user ids example
+user_ids = ["sdsdsfsfsfasfasfasfasfa","23124234234","sdfsdfsdf452535"]
 try:
     while True:
         # messages as bytes
         message={
             "event_id" : event_id,
-            "user_id" : random.randint(100,1000),
+            "user_id" : random.choice(user_ids),
             "action" : random.choice(actions),
             "timestamp" : time.time(),
         }
         print(f"Sending message : {message}")
         # send to events topic
-        producer.send("events",value=message)
+        producer.send("events",value=message,key=message["user_id"])
         event_id += 1
-        time.sleep(1)
+        time.sleep(0.5)
 except KeyboardInterrupt:
     print("Producer stopped..")
 finally:
