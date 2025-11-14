c31cb228e440ef2ef4374fa1145ce3dd42e3ff62
ishworii <ishworkhanal21@gmail.com>
2025-11-14 01:07:16 -0500

    added json producer n consumer

--- consumer.py
+++ consumer.py
@@ -1,4 +1,8 @@
 from kafka import KafkaConsumer
+import json
+
+def json_deserializer(data):
+    return json.loads(data.decode("utf-8"))
 
 
 consumer = KafkaConsumer(
@@ -6,12 +10,15 @@ consumer = KafkaConsumer(
     bootstrap_servers="localhost:9092",
     auto_offset_reset="earliest",
     group_id="event-watchers",
+    value_deserializer=json_deserializer,
 )
 print("Starting consumer ...Waiting for messages..")
 print("-" * 20)
 try:
     for message in consumer:
-        print(f"Received: {message.value.decode('utf-8')}")
+        event_data = message.value
+        print(f"RECV(Partition {message.partition}):{event_data}")
+        print(f"->User {event_data['user_id']} performed action:{event_data['action']}")
 except KeyboardInterrupt:
     print("\nConsumer stopped..")
 finally:
--- producer.py
+++ producer.py
@@ -1,19 +1,31 @@
 import time
 from kafka import KafkaProducer
+import json
+import random
 
-producer = KafkaProducer(bootstrap_servers="localhost:9092")
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092",value_serializer=json_serializer)
 
 
 print("Starting producer..")
 event_id = 1
+# actions example
+actions = ["click","view","add_to_cart","purchase"]
 
 try:
     while True:
         # messages as bytes
-        message= f"Event #{event_id}: Timestamp:{time.time()}"
+        message={
+            "event_id" : event_id,
+            "user_id" : random.randint(100,1000),
+            "action" : random.choice(actions),
+            "timestamp" : time.time(),
+        }
         print(f"Sending message : {message}")
         # send to events topic
-        producer.send("events",value=message.encode("utf-8"))
+        producer.send("events",value=message)
         event_id += 1
         time.sleep(1)
 except KeyboardInterrupt:
